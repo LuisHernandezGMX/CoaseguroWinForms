9af592342793d00be37179700fdbada33af5490d
hernandl <luis.hernandez@gmx.com.mx>
2018-12-14 11:48:38 -0600

    Entity Framework y CoaseguroContext agregados al proyecto. Conexión exitosa con BD extraída de la cadena de conexión del SII.

---
+++ CoaseguroWinForms/DAL/Entities/CoaseguroContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoaseguroWinForms.DAL.Entities
+{
+    /// <summary>
+    /// Conexión por Entity Framework a la base de datos de acuerdo
+    /// a la cadena de conexión del SII.
+    /// </summary>
+    public class CoaseguroContext : DbContext
+    {
+        /// <summary>
+        /// Genera una nueva conexión a la base de datos indicada en la cadena
+        /// de conexión.
+        /// </summary>
+        /// <param name="connectionString">La cadena de conexión descifrada del SII.</param>
+        public CoaseguroContext(string connectionString) : base(connectionString)
+        {
+            Database.SetInitializer<CoaseguroContext>(null);
+        }
+    }
+}
